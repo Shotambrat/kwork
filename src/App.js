d2f9f619ba250092e2a39a0d22b94bae21ee4f1b
Abdukhakim <dudumikah@gmail.com>
2025-05-04 22:14:05 +0500

    feat: ff

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ function App() {
         <p>
         Может перейдем в тг для удобства общения?))
         @El_Guseia
-
+        </p>
+        <p style={{fontSize: "20px", color: "rgba(0,0,0,0.5)"}}>
+            *А то в Kwork нельзя писать про то что намерены перейти на другую платформу, могут забанить
         </p>
 
         <img src={"/el_guseia.jpg"} alt="My Avatar" className={"App-logo"}/>
